64c8e31e7b236861ba37f2730f23372c3567e1ab
Rakibhasaniue <rakibhasanoyoniu@gmail.com>
2023-01-31 21:08:33 +0600

    create rest api method

--- app.js
+++ app.js
@@ -1,10 +1,37 @@
 const express = require('express');
+const fs = require('fs');
 const port = 3000;
 
 const app = express();
+app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello from the server side!');
+  res
+    .status(200)
+    .json({ Message: 'Hello from the server side!', App: 'Natours' });
+});
+
+// app.post('/', (req, res) => {
+//   res.send('You Can Post To This Endpoint');
+// });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    result: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  console.log(req.body);
+  res.send('Done');
 });
 
 app.listen(port, () => {
